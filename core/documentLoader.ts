ea49ba1e37575525a8334b1663d3865365c4622b
Orie Steele <orie@transmute.industries>
2021-10-23 22:52:51 -0500

    chore: copy from didmeme

---
+++ core/documentLoader.ts
@@ -0,0 +1,32 @@
+import {
+  documentLoaderFactory,
+  contexts,
+} from "@transmute/jsonld-document-loader";
+
+import { resolvers } from "./resolvers";
+
+import { contexts as localContexts } from "./contexts";
+
+export const documentLoader = documentLoaderFactory.pluginFactory
+  .build({
+    contexts: {
+      ...contexts.W3C_Decentralized_Identifiers,
+      ...contexts.W3C_Verifiable_Credentials,
+      ...contexts.W3ID_Security_Vocabulary,
+    },
+  })
+  .addContext(localContexts)
+  .addResolver({
+    // eslint-disable-next-line
+    ["did:key:z6M"]: {
+      resolve: resolvers.ed25519,
+    },
+  })
+  .addResolver({
+    // eslint-disable-next-line
+    ["did:meme:"]: {
+      resolve: resolvers.meme,
+    },
+  })
+
+  .buildDocumentLoader();
