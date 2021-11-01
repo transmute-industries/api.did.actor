db193affbfd1af8230cebd9e431f511a5a559da5
Orie Steele <orie@transmute.industries>
2021-10-31 20:41:47 -0500

    chore: add context

--- core/contexts/index.ts
+++ core/contexts/index.ts
@@ -5,6 +5,8 @@ import jwsv1 from "./jws-v1.json";
 import revv1 from "./rev-v1.json";
 import ed25519v1 from "./ed25519-v1.json";
 import x25519v1 from "./x25519-v1.json";
+import secv2 from "./sec-v2.json";
+import secv1 from "./sec-v1.json";
 
 export const contexts: any = {
   "https://www.w3.org/2018/credentials/v1": credsv1,
@@ -13,4 +15,7 @@ export const contexts: any = {
   "https://w3id.org/vc-revocation-list-2020/v1": revv1,
   "https://w3id.org/security/suites/ed25519-2018/v1": ed25519v1,
   "https://w3id.org/security/suites/x25519-2019/v1": x25519v1,
+
+  "https://w3id.org/security/v2": secv2,
+  "https://w3id.org/security/v1": secv1,
 };
