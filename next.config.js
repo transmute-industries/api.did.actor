bac246109b7c1a47f4dd895edd8dc63e807a878d
Orie Steele <orie@transmute.industries>
2022-01-26 17:28:06 -0600

    chore: test cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
