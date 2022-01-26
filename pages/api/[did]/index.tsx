96eab92c00ca5644333ba52956883ba129a2d2ea
Orie Steele <orie@transmute.industries>
2022-01-26 16:09:04 -0600

    chore: test cors

--- pages/api/[did]/index.tsx
+++ pages/api/[did]/index.tsx
@@ -2,8 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getResolutionResult } from "../../../core/getResolutionResult";
 
-import cors from "../../../middleware/cors";
-
 type Data = {
   didDocument: any;
   didResolutionMetadata: any;
@@ -14,7 +12,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  await cors(req, res);
   const { did } = req.query;
   const result = await getResolutionResult(did as string);
   res.status(200).json(result);
