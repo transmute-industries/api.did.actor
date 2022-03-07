6e23e87fef29b4e0f4b85965c70f246491cb474d
Orie Steele <orie@transmute.industries>
2022-03-07 15:23:53 -0600

    chore: add examples

--- core/resolutionWithEthereum.ts
+++ core/resolutionWithEthereum.ts
@@ -6,27 +6,29 @@ export const resolutionWithEthereum = async (resolution: any) => {
   if (!resolution.didDocument.verificationMethod) {
     return resolution;
   }
-  const addresses = await Promise.all(
-    resolution.didDocument.verificationMethod
-      .map(async (vm: any) => {
-        if (vm.publicKeyJwk.crv === "secp256k1") {
-          const kp = await didKey.secp256k1.Secp256k1KeyPair.from(vm);
-          const fingerprint = await kp.fingerprint();
-          const did = `did:key:${fingerprint}`;
-          const { didDocument } = await didKey.resolve(did, {
-            accept: "application/did+ld+json",
-          });
-          const publicKey = bs58.decode(
-            didDocument.verificationMethod[0].publicKeyBase58
-          );
-          const address = publicKeyToAddress(Buffer.from(publicKey));
-          return address;
-        }
-      })
-      .filter((vm: any) => vm)
+  let addresses = await Promise.all(
+    resolution.didDocument.verificationMethod.map(async (vm: any) => {
+      if (vm.publicKeyJwk.crv === "secp256k1") {
+        const kp = await didKey.secp256k1.Secp256k1KeyPair.from(vm);
+        const fingerprint = await kp.fingerprint();
+        const did = `did:key:${fingerprint}`;
+        const { didDocument } = await didKey.resolve(did, {
+          accept: "application/did+ld+json",
+        });
+        const publicKey = bs58.decode(
+          didDocument.verificationMethod[0].publicKeyBase58
+        );
+        const address = publicKeyToAddress(Buffer.from(publicKey));
+        return address;
+      }
+    })
   );
-  resolution.didDocumentMetadata.ethereum = {
-    accounts: addresses,
-  };
+  addresses = addresses.filter(Boolean);
+  if (addresses.length) {
+    resolution.didDocumentMetadata.ethereum = {
+      accounts: addresses,
+    };
+  }
+
   return resolution;
 };
