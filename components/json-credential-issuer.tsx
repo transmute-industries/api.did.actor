7016a143a4cf383845b3bbc912433ea61b2cf4a4
Orie Steele <orie@transmute.industries>
2022-08-11 13:37:24 -0500

    Better presentation links

--- components/json-credential-issuer.tsx
+++ components/json-credential-issuer.tsx
@@ -147,6 +147,7 @@ const JsonCredentialIssuer = ({ value }: any) => {
               <Button
                 color="secondary"
                 variant="contained"
+                onClick={handleIssue}
                 endIcon={<CreateIcon />}
               >
                 Issue Credential
